821836da2fe7f76ca3d2a2c08f54c29cbd0807af
crimaco197 <crmartinezco@unal.edu.co>
2018-09-10 00:05:05 -0500

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fibonacci;
 
 import java.util.Scanner;
@@ -11,7 +6,7 @@ import java.lang.*;
 
 /**
  *
- * @author LauraB
+ * @author CristianMartinez
  */
 public class Fibonacci {
 
